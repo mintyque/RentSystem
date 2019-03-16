01ff72267e45e851f4f72276bd6d6c5b6940f8fa
mintyque <bochkaryov.pavlodar@gmail.com>
2019-03-16 14:45:46 +0300

    Added User entity and User repository TODO: UserController, UserService, maybe some exceptions???

---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+    User findUserByEmail(String email);
+
+    User findUserById(Integer id);
+
+    void removeUserById(Integer id);
+}
